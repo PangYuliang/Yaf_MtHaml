0e082100e235c185664a29a03f8fa40ff16a2bd2
aninePang <anineok@163.com>
2018-05-17 10:44:23 +0800

    add new line

--- index.php
+++ index.php
@@ -3,3 +3,5 @@ define("APP_PATH",realpath(dirname(__FILE__).'/'));
 define("CONFIG_PATH",APP_PATH.'/config/');
 $app = new Yaf_Application(APP_PATH.'/config/application.ini');
 $app->bootstrap()->run();
+
+$a = 'c';
